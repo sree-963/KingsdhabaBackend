d697c8088d88ef207b4b38146c0f767862bf30bf
Bhanu <bhanuchander.m@iprismtech.com>
2023-03-14 14:19:21 +0530

    .

---
+++ routes/categoryRoute.js
@@ -0,0 +1,49 @@
+const express = require("express");
+const categoryRoute = express.Router();
+const isAdminAuth = require("../middleware/adminAuth");
+const {
+  getAllCategories,
+  getAllCategoriesWithProducts,
+  createCategoryWithImage,
+  deleteCategory,
+  getSingleCategory,
+  updateCategory,
+  toggleCategoryStatus
+} = require("./../controllers/categoryController");
+const uploadImage = require("../utils/multer");
+
+//routes
+
+// create category
+categoryRoute.post(
+  "/create-category",
+  isAdminAuth,
+  uploadImage.single("avatar"),
+  createCategoryWithImage
+);
+
+//update category
+categoryRoute.patch(
+  "/update-category/:id",
+  isAdminAuth,
+  uploadImage.single("avatar"),
+  updateCategory
+);
+
+//getALl category
+categoryRoute.get("/get-category", getAllCategories);
+
+//get all categories with products
+categoryRoute.get("/get-category-with-products", getAllCategoriesWithProducts);
+
+//updateStatus 
+categoryRoute.patch("/single-category/toggleStatus/:id",isAdminAuth, toggleCategoryStatus);
+
+
+//single category
+categoryRoute.get("/single-category/:id", getSingleCategory);
+
+//delete category
+categoryRoute.delete("/delete-category/:id", isAdminAuth, deleteCategory);
+
+module.exports = categoryRoute;
