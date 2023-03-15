1a299d8f95072de4f2ccb3a7c034ac74ed52b76f
Bhanu <bhanuchander.m@iprismtech.com>
2023-03-15 18:59:57 +0530

    .

--- conifg/dbFile.js
+++ conifg/dbFile.js
@@ -1,8 +1,8 @@
 const mongoose = require("mongoose");
 mongoose.set("strictQuery", true);
 mongoose
-  .connect("mongodb+srv://dhaba:dhaba@dhaba.zqk3rj4.mongodb.net/test")
-  // .connect('mongodb://127.0.0.1:27017/dhaba')
+  // .connect("mongodb+srv://dhaba:dhaba@dhaba.zqk3rj4.mongodb.net/test")
+  .connect('mongodb://127.0.0.1:27017/dhaba')
   .then(() => {
     console.log(`db connected`);
   })
